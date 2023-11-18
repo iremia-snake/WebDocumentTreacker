f7465cbbacfab892a070e4cb520c9def9e31080c
Пётр Черкашин <cherkashinpp@gmail.com>
2023-11-18 18:15:28 +0900

    Made migration

--- WebDocumentTracker/views.py
+++ WebDocumentTracker/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def auth(request):
     data = {
-        'values':[1,2,3,4,5,6,7,8,9,0]
+
     }
     return render(request, 'WebDocumentTracker/authorization.html', data)
 def index(request):
